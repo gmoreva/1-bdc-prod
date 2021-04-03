99cc0afcc4d156e24fc8f5d56bf1cdf2e0134600
Anton Morev <shikto@mail.ru>
2021-04-03 19:56:27 +0300

    Update all fix

--- updateAll.sh
+++ updateAll.sh
@@ -21,12 +21,9 @@ curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="start deploy ${BASE_DOMAIN} ${bran
 https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
 
 folders=(\
-    $BALANCE_SRC_DIR \
+    $SERVICE_1_SRC_DIR \
+    $SERVICE_2_SRC_DIR \
     $INTERFACE_SRC_DIR \
-    $ORDERBOOK_SRC_DIR \
-    $SOCKETSERVER_SRC_DIR \
-    $WALLET_SRC_DIR \
-    $ADMIN_SRC_DIR \
       )
 
 for folder in ${folders[*]}
@@ -37,8 +34,6 @@ do
 done
 
 cd $prodDir && docker-compose up -d --build
-cd $prodDir && docker-compose exec socket npm start db.migrate
-cd $prodDir && docker-compose up -d
 
 curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="finish deploy ${BASE_DOMAIN}" \
 https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
